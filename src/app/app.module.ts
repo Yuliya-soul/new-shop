a3be7207a31543a98f72f91a41f77841285d6323
Your Name <darki@tut.by>
2022-03-30 23:04:22 +0300

    add:task1 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './first-component/first-component.component';
+import { ProductComponentComponent } from './product-component/product-component.component';
+import { ProductListComponentComponent } from './product-list-component/product-list-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponentComponent
+    FirstComponentComponent,
+    ProductComponentComponent,
+    ProductListComponentComponent
   ],
   imports: [
     BrowserModule
