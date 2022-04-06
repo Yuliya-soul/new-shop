1cfe428e068450d0e53a8b5ded89bc16b35da403
Your Name <darki@tut.by>
2022-04-06 21:27:06 +0300

    fix:cart sevice

--- src/app/cart-list/cart.service.ts
+++ src/app/cart-list/cart.service.ts
@@ -5,19 +5,13 @@ import { IBook } from '../product/product.model';
 @Injectable()
 export class CartService {
   condition: boolean = true;
-  totalCost!: number;
-  totalQuantity!: number;
   private channel = new Subject<IBook>();
   public channel$ = this.channel.asObservable();
-  
-  publishData(data: any):any {
-    this.totalCost =this.totalCost+data.price;
-    this.totalQuantity++;
-    return  this.channel.next(data);
+
+  publishData(data: any): any {
+    return this.channel.next(data);
   }
   toggle() {
     return (this.condition = !this.condition);
   }
-
- 
 }
