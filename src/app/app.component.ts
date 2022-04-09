b3148d236a2011fa0dea1fd2805ad649b8a5de5a
Your Name <darki@tut.by>
2022-04-09 18:45:22 +0300

    add:task 2.9-2.11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'new-shop';
+  @ViewChild('appTitle', { static: false })
+  nameParagraph!: ElementRef;
+  ngAfterViewInit() {
+    this.change();
+  }
+  change() {
+    if (this.nameParagraph !== undefined) {
+      this.nameParagraph.nativeElement.textContent = 'Welcome to our shop ';
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,17 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { CartModule } from './cart-list/cart.module';
 import { ProductsModule } from './product/product.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent, ProductListComponent],
-  imports: [BrowserModule, CartModule, ProductsModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    CartModule,
+    ProductsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
