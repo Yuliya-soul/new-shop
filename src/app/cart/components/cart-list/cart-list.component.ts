c7af70f9845346dda3dc11ee21eb4b477403dae8
Your Name <darki@tut.by>
2022-05-24 17:46:04 +0300

    add:increaseQuantity()/decreaseQuantity(),changeQuantity() at cartService

--- src/app/cart/components/cart-list/cart-list.component.ts
+++ src/app/cart/components/cart-list/cart-list.component.ts
@@ -14,10 +14,10 @@ export class CartListComponent implements OnInit {
   private sub!: Subscription;
   input!: IBook;
   condition: boolean = true;
-  totalQuantity = 0;
-  totalCost = 0;
   isVerdana = true;
   isNavy = true;
+  totalQuantity = 0;
+  totalCost = 0;
 
   currentClasses = {
     verdanaFont: this.isVerdana,
@@ -25,57 +25,29 @@ export class CartListComponent implements OnInit {
   };
 
   ngOnInit(): void {
-    this.sub = this.cartService.channel$.subscribe((data: IBook) => {
-      this.input = data;
-      if (this.books.includes(this.input)) {
-        let indexOfOrderedBook = this.books.indexOf(this.input);
-        this.books[indexOfOrderedBook].quantity++;
-      } else {
-        this.books.push(this.input);
-        let indexOfOrderedBook = this.books.indexOf(this.input);
-        this.books[indexOfOrderedBook].quantity++;
-      }
-      this.totalQuantity++;
-      this.totalCost = this.totalCost + this.input.price;
-    });
+    this.cartService.getProducts();
+    this.books = this.cartService.booksInBucket;
   }
   ngOnDestroy(): void {
     this.sub.unsubscribe();
   }
   toggle(): void {
     this.condition = this.cartService.toggle();
+    this.totalQuantity = this.cartService.totalQuantity;
+    this.totalCost = this.cartService.totalCost;
   }
   trackByBooks(index: number, item: IBook): number {
     return item.id;
   }
 
   onChanged(increased: boolean, item: IBook) {
-    if (this.books.includes(item)) {
-      let indexOfSelectedBook = this.books.indexOf(item);
-      if (increased == true) {
-        this.books[indexOfSelectedBook].quantity++;
-        this.totalQuantity++;
-        this.totalCost = this.totalCost + item.price;
-      } else {
-        if (this.books[indexOfSelectedBook].quantity != 0) {
-          this.books[indexOfSelectedBook].quantity--;
-          this.totalQuantity--;
-          this.totalCost = this.totalCost - item.price;
-        }
-        if (this.books[indexOfSelectedBook].quantity === 0) {
-          this.books.splice(indexOfSelectedBook, 1);
-          this.totalQuantity = this.totalQuantity - item.quantity;
-        }
-      }
-    }
+    this.cartService.changeQuantity(increased, item);
+    this.totalQuantity = this.cartService.totalQuantity;
+    this.totalCost = this.cartService.totalCost;
   }
   onDeleted(item: IBook) {
-    let indexOfSelectedBook = this.books.indexOf(item);
-    this.totalQuantity = this.totalQuantity - item.quantity;
-    this.totalCost = this.totalCost - item.price * item.quantity;
-    this.books[indexOfSelectedBook].quantity = 0;
-    this.books.splice(indexOfSelectedBook, 1);
+    this.cartService.removeProduct(item);
+    this.totalQuantity = this.cartService.totalQuantity;
+    this.totalCost = this.cartService.totalCost;
   }
-
-
 }
--- src/app/products/components/product/product.component.ts
+++ src/app/products/components/product/product.component.ts
@@ -11,7 +11,6 @@ import { ProductService } from '../../services/product.service';
 })
 export class ProductComponent implements OnInit {
   books: IBook[] = [];
-  list: IBook[] = [];
 
   constructor(
     private dataService: ProductService,
@@ -27,7 +26,7 @@ export class ProductComponent implements OnInit {
   }
 
   onAddToCart(item: IBook): void {
-    this.list.push(this.cartService.publishData(item));
+    this.cartService.publishData(item);
   }
 
   addDescription(data: IBook): void {
