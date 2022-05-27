571b58eaacc14a4464a91573e87833d444a6747d
Your Name <darki@tut.by>
2022-05-27 14:26:02 +0300

    add:  GeneratorService

---
+++ src/app/core/services/generator.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class GeneratorService {
+  generate(n: number): string {
+    let text = '';
+    const possible =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < n; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+  }
+}
